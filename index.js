07282422793f9e41a19beaea39955ceef151857e
Burak Mustafa Dündar <86871573+bmdundar24@users.noreply.github.com>
2021-11-02 19:47:11 +0300

    fixed list element

--- index.js
+++ index.js
@@ -39,14 +39,11 @@ function newElement() {
     document.getElementById("myUL").appendChild(li);
   }
   document.getElementById("text").value = "";
-
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
-  
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
       var div = this.parentElement;
