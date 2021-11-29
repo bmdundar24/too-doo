4d98aadb782bc8b0963e93da8ed80bd6cda10989
Burak Mustafa Dündar <86871573+bmdundar24@users.noreply.github.com>
2021-11-29 20:14:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
-for (i = 0; i < myNodelist.length; i++) {
+for (i = 0; i < myNodelist.length; i++) { 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
   span.className = "close";
